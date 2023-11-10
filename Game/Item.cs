83d72531ba957c421908ab7a5b246fc9c6da0fff
Bende Huba <bende.huba@students.jedlik.eu>
2023-11-10 16:59:26 +0100

    Player class statok létrehozása

---
+++ Game/Item.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Game
+{
+    enum StatType
+    {
+        Health,
+        Damage,
+        SliderSpeed,
+        Key
+    }
+    abstract class Item
+    {
+        private string name;
+        private StatType type;
+        private int stat;
+        private int price;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public StatType Type
+        {
+            get { return type; }
+            set { type = value; }
+        }
+        public int Stat
+        {
+            get { return stat; }
+            set { stat = value; }
+        }
+        public int Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+    }
+    class Sword : Item
+    {
+        public Sword(string name, int stat, int price = 0, StatType type = StatType.Damage)
+        {
+            this.Name = name;
+            this.Stat = stat;
+            this.Price = price;
+            this.Type = type;
+        }
+        
+    }
+    class Armor : Item
+    {
+        public Armor(string name, int stat, int price = 0, StatType type = StatType.Health)
+        {
+            this.Name = name;
+            this.Stat = stat;
+            this.Price = price;
+            this.Type = type;
+        }
+
+    }
+
+}
---
+++ Game/Player.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    class Player
+    {
+        #region VARIABLES
+        private int health;
+        private int money;
+
+        private int defaultHealth;
+        private int defaultStrength;
+        private int defaultSliderSpeed;
+
+        public Sword sword;
+        public Armor armor;
+        #endregion
+
+        #region FIELDS
+        public Item[] Items
+        {
+            get 
+            { 
+                return new Item[2] {this.sword, this.armor};
+            }
+        }
+
+        public int Health
+        {
+            get { return health; }
+            set 
+            {
+                if (health <= value) {
+                    health = value;
+                }
+                else
+                {
+                    health = MaxHealth;
+                }
+                
+            }
+        }
+        public int Money
+        {
+            get { return money; }
+            set { money = value; }
+        }
+        public int MaxHealth
+        {
+            get
+            { 
+                return defaultHealth + calculateStat(StatType.Health);
+            }
+        }
+
+        public int SliderSpeed
+        {
+            get
+            {
+                return defaultSliderSpeed + calculateStat(StatType.SliderSpeed);
+            }
+        }
+
+        public int Damage
+        {
+            get
+            {
+                return defaultStrength + calculateStat(StatType.Damage);
+            }
+        }
+        #endregion
+
+        public Player(int health, int strength, int sliderSpeed, int money)
+        {
+            this.defaultHealth = health;
+            this.defaultStrength = strength;
+            this.defaultSliderSpeed = sliderSpeed;
+            this.money = money;
+        }
+
+        private int calculateStat(StatType type)
+        {
+            int stat = 0;
+            foreach (Item item in this.Items)
+            {   
+                if (item.Type == type)
+                {
+                    stat += item.Stat;
+                }
+            }
+            return stat;
+        }
+    }
+}
