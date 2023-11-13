e914cf64ef8e94e429acb1351b7a8bd6daa428ec
Kovács Péter <kovacs.peter@students.jedlik.eu>
2023-11-13 10:08:10 +0100

    webkesz

--- Game/Program.cs
+++ Game/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Console.WriteLine("Hello, World!");
+            FightSystem f = new FightSystem(new Player(500, 10, 1, 0), new Enemy(200, 10), out bool won);
         }
     }
 }
