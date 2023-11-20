dcc9ecda5f9e3bfdd0f5d8512f336a43dd31e977
Kovács Péter <kovacs.peter@students.jedlik.eu>
2023-11-20 10:31:23 +0100

    hely kezdemény

--- Game/Program.cs
+++ Game/Program.cs
@@ -2,10 +2,31 @@
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello, World!");
-            FightSystem f = new FightSystem(new Player(500, 10, 1, 0), new Enemy(200, 10), out bool won);
+            //FightSystem f = new FightSystem(new Player(500, 10, 1, 0), new Enemy(200, 10), out bool won);
+            
+
+        }
+        static void SlowPrint(string text)
+        {
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(50);
+            }
+        }
+
+        public static int hely_1()
+        {
+            
+
+
+
+
+            return (2);
         }
     }
 }
