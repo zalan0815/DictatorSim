08118bbd61dab8136476559d4b156d5b8f9e1377
zalan0815 <tofalvi.zalan@students.jedlik.eu>
2023-11-24 12:21:56 +0100

    locations kb kész

--- Game/Program.cs
+++ Game/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello, World!");
-            FightSystem f = new FightSystem(new Player(500, 10, 1, 0), new Enemy(200, 10), out bool won);
+            //FightSystem f = new FightSystem(new Player(500, 10, 1, 0), new Enemy(200, 10), out bool won);
+            //Locations.Asd();
         }
     }
 }
