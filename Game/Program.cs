abb021ba4c2a6093bc2fd53f442aeaa390c6bf7c
Kovács Péter <kovacs.peter@students.jedlik.eu>
2023-11-24 12:29:02 +0100

    print

--- Game/Program.cs
+++ Game/Program.cs
@@ -2,30 +2,42 @@
 {
     internal class Program
     {
-        
+        static Player player;        
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello, World!");
             //FightSystem f = new FightSystem(new Player(500, 10, 1, 0), new Enemy(200, 10), out bool won);
+            player = new Player(10, 10, 1, 0);
+            int printLenght = Console.WindowWidth - 30;
             
-
         }
-        static void SlowPrint(string text)
+        static void SlowPrint(string text, int printLenght)
         {
-            foreach (char c in text)
+            for (int i = 0; i < text.Length; i++)
             {
-                Console.Write(c);
-                Thread.Sleep(50);
+                if (Console.CursorLeft == printLenght)
+                {
+                    Console.Write(' ');
+                    while (text[i] != ' ')
+                    {
+                        i--;
+                        Console.CursorLeft -= 2;
+                        Console.Write(' ');
+                    }
+                    Console.WriteLine(text[i]);
+                    Thread.Sleep(25);
+                }
+                else
+                {
+                    Console.Write(text[i]);
+                    Thread.Sleep(25);
+                }
             }
+            Console.WriteLine('\n');
         }
 
         public static int hely_1()
         {
-            
-
-
-
-
+            SlowPrint("A történet a házadban veszi kezdetét, ahol egy szép napon reggel elhatározod, hogy elmész világot látni. ");
             return (2);
         }
     }
