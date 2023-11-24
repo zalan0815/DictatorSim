b3096e9dd9f0622219ebb583d303701a854a652c
Bende Huba <bende.huba@students.jedlik.eu>
2023-11-24 20:37:58 +0100

    Játékos statisztikáinak kiírása oldalra

--- Game/Program.cs
+++ Game/Program.cs
@@ -8,12 +8,10 @@
         {
             //FightSystem f = new FightSystem(new Player(500, 10, 1, 0), new Enemy(200, 10), out bool won);
             Locations.Generate();
-            player = new Player(10, 10, 1, 100);
+            player = new Player(10, 10, 1, 0);
             printLenght = Console.WindowWidth - 30;
-            Shop shop = new Shop(new Sword("Lézerkard", 10, 60, StatType.Damage), new Armor("Netherite páncél", 10, 99, StatType.Health), new Sword("Nashor foga", 2, 123, StatType.SliderSpeed), new OtherItem("Garfield hűtőmágnes", 4, -2, StatType.Key));
-            shop.ShopMenu(ref player);
         }
-        static void SlowPrint(string text, int printLenght)
+        static void SlowPrint(string text)
         {
             for (int i = 0; i < text.Length; i++)
             {
@@ -34,8 +32,46 @@
                     Console.Write(text[i]);
                     Thread.Sleep(25);
                 }
-                Console.WriteLine();
             }
+            Console.WriteLine();
+        }
+        static int getPlayerStatLength()
+        {
+            int longest = "életerőd".Length;
+            foreach (Item item in player.Items)
+            {
+                if (longest < item.WriteItemStat(true, false))
+                {
+                    longest = item.WriteItemStat(true, false);
+                }
+            }
+            longest = longest >= $"{player.Health} / {player.MaxHealth}".Length ? longest : $"{player.Health} / {player.MaxHealth}".Length;
+            return longest;
+        }
+        public static int PrintPlayerStat(bool yellowHealth = false)
+        {
+            int longest = getPlayerStatLength();
+
+            Console.CursorLeft = Console.WindowWidth - longest - 1;
+            Console.CursorTop = 0;
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("Életerőd");
+            Console.CursorLeft = Console.WindowWidth - longest - 1;
+            Console.CursorTop += 1;
+            if (yellowHealth) { Console.ForegroundColor = ConsoleColor.Yellow; }
+            Console.Write($"{player.Health} ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write($"/ {player.MaxHealth}");
+            Console.CursorTop += 1;
+            foreach (Item item in player.Items)
+            {
+                Console.CursorLeft = Console.WindowWidth - longest - 1;
+                Console.CursorTop += 1;
+                item.WriteItemStat(true);
+            }
+
+            printLenght = Console.WindowWidth - longest - 2;
+            return longest;
         }
     }
 }
