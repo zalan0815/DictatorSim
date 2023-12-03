85a70954f7671e83f6cd528a714ebd78743a47fb
Bende Huba <bende.huba@students.jedlik.eu>
2023-12-03 17:52:51 +0100

    beadott szöveg frissítve

--- Game/Program.cs
+++ Game/Program.cs
@@ -9,7 +9,7 @@ namespace Game
 
         static void Main(string[] args)
         {
-            new TyperGame(player, "vicces amongus text vagy mi").Run();
+            new TyperGame(player, "Jelenleg egy sor van tamogatva ekezetek nelkul").Run();
             Locations.Generate();
             int location = 1;
           
